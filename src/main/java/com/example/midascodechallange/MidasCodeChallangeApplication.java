1fcb213711302d61bf00dbb51d41c71b04c273fb
onderhamamcioglu <onder1hamamcioglu@gmail.com>
2023-08-31 11:41:06 +0300

    Initial Commit

---
+++ src/main/java/com/example/midascodechallange/MidasCodeChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.midascodechallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidasCodeChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidasCodeChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/midascodechallange/MidasCodeChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.midascodechallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidasCodeChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
